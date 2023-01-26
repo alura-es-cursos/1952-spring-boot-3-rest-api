ea09e98442c9bd52795beb5bc60c856fd5737aac
Diego Rojas <darguelles.rojas91@gmail.com>
2023-01-27 00:31:09 +0100

    Fix

--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,6 +1,6 @@
 package med.voll.api.medico;
 
-import med.voll.api.DatosDireccion;
+import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion direccion) {
 }
